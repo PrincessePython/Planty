cde897ac94af2ad82767c41946d3874e34c121b7
Olga <sumopetrus@gmail.com>
2022-12-08 09:42:46 +0100

    initial commit

---
+++ app/public/wp-content/themes/kadence-child/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+** activation theme
+**/
+function child_enqueue_styles() {
+	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); 
